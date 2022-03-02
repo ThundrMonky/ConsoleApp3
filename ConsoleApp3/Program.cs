56dda0b30142bee9be7a54a9306d535ad3b7d137
Pitz, Jack <jpitz@geico.com>
2022-03-01 20:22:55 -0500

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
